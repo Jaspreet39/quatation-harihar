1673086dfcab7995a75833b72079c8b07ac9eda3
Jaspreet39 <jassingh.0244@gmail.com>
2023-02-01 15:03:18 +0530

    finbal comiit

--- src/Estimate.js
+++ src/Estimate.js
@@ -1,18 +1,156 @@
-import React from "react";
+import { Label, Select, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { carcases, Drawer, Shelf, Shutter, visible } from "./constant";
 
 function Estimate() {
+  const [charge, setCharge] = useState([]);
+  const [carcase, setCarcase] = useState("");
+  const [sqFeet, setSqFeet] = useState(0);
+  const [rupee, setRupee] = useState(0);
+
+  const totalValue = (e) => {
+    const fittingCharges = sqFeet * 150;
+    const misCharges = sqFeet * 75;
+    console.log(charge);
+    setCharge((d) => [...d, e]);
+    const sum = charge.map((item) => parseInt(item)).reduce((p, a) => p + a, 0);
+    const totalAmmount = sum * sqFeet + fittingCharges + misCharges;
+    const less = 5 / 100;
+    const GST = 18 / 100;
+    const discount = totalAmmount - totalAmmount * less;
+    const totalValue = setRupee(discount + discount * GST);
+  };
+
   return (
-    <div className="h-[95vh] w-full  grid xl:grid-cols-2 md:grid-cols-2 grid-cols-1  bg-[#232949]">
-      <div className="col-span-1">
-        <h1>Easy steps to calculate quotation</h1>
-        <p>Apply for quotation in minutes</p>
-        <div className=" max-w-[40%]">
-          <select className="w-full px-2 py-3 rounded-md">
-            <option>Select brand</option>
-          </select>
+    <div className="h-[95vh] w-full  grid xl:grid-cols-2 md:grid-cols-2 grid-cols-1 space-y-4 xl:space-y-0 md:space-y-0 bg-[#232949]">
+      <div className="col-span-1 flex flex-col  justify-center items-center space-y-4">
+        <div>
+          <h1 className="xl:text-5xl md:text-3xl text-2xl  font-serif text-white">
+            Easy steps to calculate quotation
+          </h1>
+          <p className="text-white xl:text-xl md:text-lg text-sm">
+            Apply for quotation in minutes
+          </p>
+        </div>
+        <div className="xl:w-[60%] md:w-[60%] w-[100%] bg-[#30355A] px-3 py-5 space-y-3">
+          <div className="w-full space-y-2">
+            <div className="mb-2 block">
+              <Label htmlFor="sqFeet" value="Square Feet" className="text-xl" />
+            </div>
+            <TextInput
+              id="sqFeet"
+              type="number"
+              min={0}
+              placeholder="Enter SQ/Feet"
+              required={true}
+              onChange={(e) => setSqFeet(e.target.value)}
+            />
+          </div>
+          <div className="w-full space-y-2">
+            <div className="mb-2 block">
+              <Label
+                className="text-xl text-white"
+                htmlFor="carcase"
+                value="carcase"
+              />
+            </div>
+            <Select onChange={(e) => totalValue(e.target.value)} id="carcase">
+              <option value="">Select Carcases</option>
+              {carcases.map((item) => (
+                <option value={item.Price}>{item.title}</option>
+              ))}
+            </Select>
+          </div>
+          <div className="w-full space-y-2">
+            <div className="mb-2 block">
+              <Label
+                className="text-xl text-white"
+                htmlFor="shutter"
+                value="shutter"
+              />
+            </div>
+            <Select onChange={(e) => totalValue(e.target.value)} id="shutter">
+              <option>Select Shutter</option>
+              {Shutter.map((item) => (
+                <option value={item.Price}>{item.title}</option>
+              ))}
+            </Select>
+          </div>
+          <div className="w-full space-y-2">
+            <div className="mb-2 block">
+              <Label
+                className="text-xl text-white"
+                htmlFor="shelf"
+                value="shelf"
+              />
+            </div>
+            <Select id="shelf" onChange={(e) => totalValue(e.target.value)}>
+              <option>Select Shelf</option>
+              {Shelf.map((item) => (
+                <option value={item.Price}>{item.title}</option>
+              ))}
+            </Select>
+          </div>
+          <div className="w-full space-y-2">
+            <div className="mb-2 block">
+              <Label
+                className="text-xl text-white"
+                htmlFor="drawer"
+                value="Drawer"
+              />
+            </div>
+            <Select onChange={(e) => totalValue(e.target.value)} id="drawer">
+              <option>Select Drawer</option>
+              {Drawer.map((item) => (
+                <option value={item.Price}>{item.title}</option>
+              ))}
+            </Select>
+          </div>
+          <div className="w-full space-y-2">
+            <div className="mb-2 block">
+              <Label
+                className="text-xl text-white"
+                htmlFor="visible"
+                value="Visible"
+              />
+            </div>
+            <Select onChange={(e) => totalValue(e.target.value)} id="visible">
+              <option>Select Visibles</option>
+              {visible.map((item) => (
+                <option value={item.Price}>{item.title}</option>
+              ))}
+            </Select>
+          </div>
         </div>
       </div>
-      <div className="col-span-1"></div>
+      <div className="col-span-1 flex items-center justify-center">
+        <div className="bg-[#30355A] h-[400px] w-[400px] shadow-md xl:rounded-2xl md:rounded-2xl flex flex-col items-center justify-center space-y-4">
+          <div className="space-y-2">
+            <div className="flex items-center justify-around">
+              {charge.map((item) => (
+                <p className="px-1 bg-gray-50 rounded-md">{item}</p>
+              ))}
+            </div>
+            <h1 className="text-2xl text-white font-serif font-semibold">
+              Total Payment is
+            </h1>
+            <p className="text-2xl text-white font-serif font-semibold text-center">
+              Rs.<spam className="font-normal">{rupee}</spam>
+            </p>
+          </div>
+          <div className="h-[1px] w-[70%] bg-white" />
+          <div className="flex items-center justify-between space-x-10">
+            <div>
+              <h1 className="text-xl text-white font-serif ">Discount</h1>
+              <p className="text-lg text-white font-serif ">10%</p>
+            </div>
+            <div>
+              <h1 className="text-xl text-white font-serif ">GST</h1>
+              <p className="text-lg text-white font-serif ">18%</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/constant.js
@@ -0,0 +1,243 @@
+export const carcases = [
+  {
+    title:
+      "Action HDHMR PRELAM BSL Connection via Mini—fix(withouthardware) matching tape",
+    Price: 600,
+  },
+  {
+    title:
+      "Action HDHMR POSTLAM with Liner (0.7) pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 625,
+  },
+  {
+    title:
+      "MR Ply with Liner (0.7) Pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 600,
+  },
+  {
+    title:
+      "BWP Ply with Liner (0.7)pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 700,
+  },
+  {
+    title:
+      "Action Boilo with Liner (0.7) pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 875,
+  },
+  {
+    title:
+      "Other Brand HDHMR PHhLAM BSB connection Via Mini-fix(without hardware) matching tape",
+    Price: 525,
+  },
+  {
+    title:
+      "ACTION MDF PRELAM Connection via Mini-fix(without hardware) matching tape",
+    Price: 460,
+  },
+  {
+    title:
+      "ACTION Particle PRELAM Connection via Mini-fix(without hardware) matching tape",
+    Price: 400,
+  },
+];
+
+export const Shutter = [
+  {
+    title: "Marino Laminate SF Pasted on HDF with Matching Edge Banding",
+    Price: 300,
+  },
+  {
+    title: "Marino Laminate Hi-Gloss Pasted on HDF with Matching Edge Banding",
+    Price: 320,
+  },
+  {
+    title: "Marino Laminate MR+ Pasted on HDF with Matching Edge Banding",
+    Price: 340,
+  },
+  {
+    title: "Marino Laminate LUHVI Pasted on HDF with Matching Edge Banding",
+    Price: 510,
+  },
+  {
+    title:
+      "Marino Laminate VNR/Texture Pasted on HDF with Matching Edge Banding",
+    Price: 360,
+  },
+  {
+    title: "Marino Gloss Meister(Gloss) with Matching Edge Banding",
+    Price: 410,
+  },
+  {
+    title: "Marino Gloss Meister(Matt) with Matching Edge Banding",
+    Price: 440,
+  },
+  {
+    title: "Action UV with Matching Edge Banding",
+    Price: 380,
+  },
+  {
+    title: "Action Acrylic with Matching Edge Banding",
+    Price: 480,
+  },
+  {
+    title: "ORNARE BASIC with Matching Edge Banding",
+    Price: 605,
+  },
+  {
+    title: "ORNARE MATALIC with Matching Edge Banding",
+    Price: 650,
+  },
+  {
+    title: "ORNARE PREMIUM with Matching Edge Banding",
+    Price: 690,
+  },
+  {
+    title: "MDF OSL Plain For Paint without Design",
+    Price: 160,
+  },
+  {
+    title: "HDHMR OSL Plain for For Paint without Design",
+    Price: 200,
+  },
+  {
+    title: "HDHMR  Plain for For Paint without Design",
+    Price: 260,
+  },
+  {
+    title: "CNC DESIGN CHARGES (material extra)",
+    Price: 210,
+  },
+  {
+    title: "Profile Shutter Without Glass (ONLY LABOUR CHARGE)",
+    Price: 1000,
+  },
+];
+
+export const Shelf = [
+  {
+    title:
+      "Action HDHMR PRELAM BSL Connection via Mini—fix(withouthardware) matching tape",
+    Price: 200,
+  },
+  {
+    title:
+      "Action HDHMR POSTLAM with Liner (0.7) pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 225,
+  },
+  {
+    title:
+      "MR Ply with Liner (0.7) Pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 200,
+  },
+  {
+    title:
+      "BWP Ply with Liner (0.7)pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 240,
+  },
+  {
+    title:
+      "Action Boilo with Liner (0.7) pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 275,
+  },
+  {
+    title:
+      "Other Brand HDHMR PHhLAM BSB connection Via Mini-fix(without hardware) matching tape",
+    Price: 175,
+  },
+  {
+    title:
+      "ACTION MDF PRELAM Connection via Mini-fix(without hardware) matching tape",
+    Price: 150,
+  },
+  {
+    title:
+      "ACTION Particle PRELAM Connection via Mini-fix(without hardware) matching tape",
+    Price: 135,
+  },
+];
+
+export const Drawer = [
+  {
+    title:
+      "Action HDHMR PRELAM BSL Connection via Mini—fix(withouthardware) matching tape",
+    Price: 200,
+  },
+  {
+    title:
+      "Action HDHMR POSTLAM with Liner (0.7) pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 225,
+  },
+  {
+    title:
+      "MR Ply with Liner (0.7) Pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 200,
+  },
+  {
+    title:
+      "BWP Ply with Liner (0.7)pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 240,
+  },
+  {
+    title:
+      "Action Boilo with Liner (0.7) pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 275,
+  },
+  {
+    title:
+      "Other Brand HDHMR PHhLAM BSB connection Via Mini-fix(without hardware) matching tape",
+    Price: 175,
+  },
+  {
+    title:
+      "ACTION MDF PRELAM Connection via Mini-fix(without hardware) matching tape",
+    Price: 150,
+  },
+  {
+    title:
+      "ACTION Particle PRELAM Connection via Mini-fix(without hardware) matching tape",
+    Price: 135,
+  },
+];
+
+export const visible = [
+  {
+    title:
+      "Action HDHMR PRELAM BSL Connection via Mini—fix(withouthardware) matching tape",
+    Price: 400,
+  },
+  {
+    title:
+      "Action HDHMR POSTLAM with Liner (0.7) pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 400,
+  },
+  {
+    title:
+      "MR Ply with Liner (0.7) Pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 400,
+  },
+  {
+    title:
+      "BWP Ply with Liner (0.7)pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 460,
+  },
+  {
+    title:
+      "Action Boilo with Liner (0.7) pasted on both side Connection via Mini-fix(without hardware) matching tape",
+    Price: 600,
+  },
+  {
+    title:
+      "Other Brand HDHMR PHhLAM BSB connection Via Mini-fix(without hardware) matching tape",
+    Price: 350,
+  },
+  {
+    title:
+      "ACTION MDF PRELAM Connection via Mini-fix(without hardware) matching tape",
+    Price: 310,
+  },
+  {
+    title:
+      "ACTION Particle PRELAM Connection via Mini-fix(without hardware) matching tape",
+    Price: 265,
+  },
+];
